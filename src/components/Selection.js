9520332980e1bfca2c1e1b73e3e4ef6ba5edcce8
Vishal Suman <119152809+vishal3156@users.noreply.github.com>
2023-05-08 17:30:30 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,11 @@
+import "./styles/Child.css";
+const Selection = ({ applyColor }) => {
+  return (
+    <div className="fix-box" style={{ applyColor }}>
+      <h2 className="subheading">Selection</h2>
+    </div>
+  );
+};
+
+export default Selection;
 
